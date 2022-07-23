517de8c342b4660a2fa6fd923d88104eddfec03f
Luyao Han <luyaohan1001@gmail.com>
2022-07-23 09:16:37 -0400

    init commit

---
+++ download/README.md
@@ -0,0 +1,1 @@
+# This folder contains the original SDK that may serve as reference if the decompressed SDK has been polluted with my own code. 
---
+++ nRF5_SDK_17.1.0_ddde560/examples/ble_peripheral/ble_app_blinky/pca10040/s132/armgcc/_build/nrf52832_xxaa/utf.c.d
@@ -0,0 +1,8 @@
+_build/nrf52832_xxaa/utf.c.o: \
+ ../../../../../../external/utf_converter/utf.c \
+ ../../../../../../external/utf_converter/utf.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h
+../../../../../../external/utf_converter/utf.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
